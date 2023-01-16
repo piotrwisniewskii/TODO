90d83f9317ff40f2f55bd899869742d1ecd30617
Piotr Wisniewski <piowisni@gmail.com>
2023-01-16 19:55:45 +0100

    initial models created

---
+++ Models/Note.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TODO.Data.Enums;
+
+namespace TODO.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string PictureURL { get; set; }
+
+        [Required(ErrorMessage = "Please provide Task Name")]
+        [StringLength(15, MinimumLength = 4)]
+        public string NoteName { get; set; }
+
+        [Required(ErrorMessage = "Please provide Task Message")]
+        [StringLength(200, MinimumLength = 4)]
+        public string NoteMessage { get; set; }
+
+        [Required(ErrorMessage = "Please provide Task Date")]
+        public DateTime NoteDate { get; set; }
+
+
+        public Priority Priority { get; set; }
+    }
+}
---
+++ Models/TODOUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TODO.Models
+{
+    public class TODOUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
