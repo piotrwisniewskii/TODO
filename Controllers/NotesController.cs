dfbd9db1c55e1574ba8f7e7f3df4873a1c5ee417
Piotr Wisniewski <piowisni@gmail.com>
2023-01-16 21:25:39 +0100

    Controllers added

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TODO.Data.DataBase;
+
+namespace TODO.Controllers
+{
+    public class NotesController : Controller
+    {
+       private readonly TODODbContext _context;
+       public NotesController(TODODbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Notes.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ Controllers/TODOUsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TODO.Data.DataBase;
+
+namespace TODO.Controllers
+{
+    public class TODOUsersController : Controller
+    {
+        private readonly TODODbContext _context;
+
+        public TODOUsersController(TODODbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.TODOUsers.ToList();
+            return View();
+        }
+    }
+}
