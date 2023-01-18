a8ee072c6b99316dae7a6c1b0622e805f509eef1
Piotr Wisniewski <piowisni@gmail.com>
2023-01-18 19:27:54 +0100

    added Controller methods

--- Controllers/NotesController.cs
+++ Controllers/NotesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TODO.Data.DataBase;
+using TODO.Data.Models;
 using TODO.Services.NotesServices;
 
 namespace TODO.Controllers
@@ -22,5 +23,44 @@ namespace TODO.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("NoteName", "NoteMessage", "NoteDate", "Priority", "IsDone")]Note note)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(note);
+            }
+            await _service.AddAsync(note);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var modelDetails = await _service.GetByIdAsync(id);
+
+            if (modelDetails == null) return View("NotFound");
+            return View(modelDetails); 
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var modelDetails = await _service.GetByIdAsync(id);
+            if (modelDetails == null) return View("NotFound");
+            return View(modelDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,[Bind("Id,NoteName", "NoteMessage", "NoteDate", "Priority","IsDone")] Note note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(note);
+            }
+            await _service.UpdateAsync(id,note);
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
